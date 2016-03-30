2c37848083016893adda5a8dd2775d0a3e8f6022
Raven Iqqe <raviqqe@gmail.com>
2016-03-31 00:07:40 +0900

    Fix a typo

--- nn/linear.py
+++ nn/linear.py
@@ -6,6 +6,6 @@ def linear(x, output_layer_size, regularizer_scale=1e-8):
   return tf.contrib.layers.fully_connected(
     x,
     output_layer_size,
-    activation_fn=tf.elu,
+    activation_fn=tf.nn.elu,
     weight_regularizer=tf.contrib.layers.l2_regularizer(regularizer_scale),
   )

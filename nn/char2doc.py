8022515a19a4039c0d83df7ea684375dd1065957
Raven Iqqe <raviqqe@gmail.com>
2016-03-30 14:47:03 +0900

    Detail char2doc model

--- nn/char2doc.py
+++ nn/char2doc.py
@@ -1,6 +1,22 @@
 import tensorflow as tf
 
+from .id_to_embedding import id_to_embedding
+from .embeddings_to_embedding import embeddings_to_embedding
+from .linear import linear
+from . import dropout as do
 
 
-def char2doc(document, true_labels):
-  return document_embedding
+
+def char2doc(document,
+             true_labels,
+             dropout_ratio,
+             hidden_layer_size,
+             output_layer_size):
+  char_embeddings = id_to_embedding(document)
+  document_embedding = embeddings_to_embedding(char_embeddings)
+
+  def dropout(x):
+    return do.dropout(x, dropout_ratio)
+
+  hidden_layer = dropout(linear(document_embedding, hidden_layer_size))
+  return dropout(linear(hidden_layer, output_layer_size))

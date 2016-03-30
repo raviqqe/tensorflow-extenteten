0f38499ec6350beaaa2294c7286793a4f17ef257
Raven Iqqe <raviqqe@gmail.com>
2016-03-31 02:12:28 +0900

    Implement linear layer by myself
    tensorflow.contrib.layers.fully_connected and its derivates don't work
    as expected. Am I missing something?

--- nn/linear.py
+++ nn/linear.py
@@ -1,11 +1,20 @@
 import tensorflow as tf
 
+from . import var_init
+
 
 
 def linear(x, output_layer_size, regularizer_scale=1e-8):
-  return tf.contrib.layers.fully_connected(
-    x,
-    output_layer_size,
-    activation_fn=tf.nn.elu,
-    weight_regularizer=tf.contrib.layers.l2_regularizer(regularizer_scale),
-  )
+  weight = _weight([x.get_shape().as_list()[1], output_layer_size])
+  bias = _bias(output_layer_size)
+  tf.add_to_collection(tf.GraphKeys.WEIGHTS, weight)
+  tf.add_to_collection(tf.GraphKeys.BIASES, bias)
+  return tf.matmul(x, weight) + bias
+
+
+def _weight(shape):
+  return tf.Variable(var_init.normal(shape))
+
+
+def _bias(output_layer_size):
+  return tf.Variable(var_init.normal([output_layer_size]))

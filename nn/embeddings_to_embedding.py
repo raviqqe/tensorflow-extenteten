4253825d649d6be619ff902e814d89c6d5dc4a0e
Raven Iqqe <raviqqe@gmail.com>
2016-03-31 00:04:12 +0900

    Crash some bugs in nn.embeddings_to_embedding module

--- nn/embeddings_to_embedding.py
+++ nn/embeddings_to_embedding.py
@@ -5,13 +5,15 @@ import tensorflow as tf
 def embeddings_to_embedding(child_embeddings):
   assert len(child_embeddings.get_shape()) == 3
 
-  child_shape = child_embeddings.get_shape()
+  child_shape = child_embeddings.get_shape().as_list()
   batch_size = child_shape[0]
   embedding_size = child_shape[2]
   rnn_cell = tf.nn.rnn_cell.GRUCell(embedding_size, embedding_size)
 
   state = rnn_cell.zero_state(batch_size, tf.float32)
-  for child_embedding in _split_child_embeddings(child_embeddings):
+  for iteration, child_embedding \
+      in enumerate(_split_child_embeddings(child_embeddings)):
+    if iteration != 0: tf.get_variable_scope().reuse_variables()
     parent_embedding, state = rnn_cell(child_embedding, state)
   return parent_embedding
 
@@ -20,10 +22,16 @@ def _split_child_embeddings(child_embeddings):
   child_shape = child_embeddings.get_shape()
   num_of_childs = child_shape[1]
   permutation = [1, 0] + _dimension_indices(child_shape)[2:]
-  return tf.split(0,
-                  num_of_childs,
-                  tf.transpose(child_embeddings, permutation))
+  splitted_child_embeddings = tf.split(
+      0,
+      num_of_childs,
+      tf.transpose(child_embeddings, permutation))
+  return map(_reshape_child_embedding, splitted_child_embeddings)
 
 
 def _dimension_indices(shape):
   return list(range(shape.ndims))
+
+
+def _reshape_child_embedding(child_embedding):
+  return tf.reshape(child_embedding, child_embedding.get_shape()[1:])

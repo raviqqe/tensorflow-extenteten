8f491f3e02e227e3f7c09982cf1c702c48d4418c
Raven Iqqe <raviqqe@gmail.com>
2016-04-02 19:06:02 +0900

    Add a name scope to char2doc model

--- nn/char2doc.py
+++ nn/char2doc.py
@@ -14,23 +14,24 @@ def char2doc(forward_document,
              dropout_prob,
              hidden_layer_size,
              output_layer_size):
-  char_embeddings = embeddings(id_space_size=char_space_size,
-                               embedding_size=char_embedding_size)
+  with tf.name_scope("char2doc"):
+    char_embeddings = embeddings(id_space_size=char_space_size,
+                                 embedding_size=char_embedding_size)
 
-  def char_ids_to_doc_embedding(document):
-    return embeddings_to_embedding(
-        ids_to_embeddings(document, char_embeddings),
-        document_embedding_size)
+    def char_ids_to_doc_embedding(document):
+      return embeddings_to_embedding(
+          ids_to_embeddings(document, char_embeddings),
+          document_embedding_size)
 
-  document_embedding = tf.concat(
-      1,
-      list(map(char_ids_to_doc_embedding,
-               [forward_document, backward_document])))
+    document_embedding = tf.concat(
+        1,
+        list(map(char_ids_to_doc_embedding,
+                 [forward_document, backward_document])))
 
-  hidden_layer = dropout(_activate(linear(_activate(document_embedding),
-                                          hidden_layer_size)),
-                         dropout_prob)
-  return linear(hidden_layer, output_layer_size)
+    hidden_layer = dropout(_activate(linear(_activate(document_embedding),
+                                            hidden_layer_size)),
+                           dropout_prob)
+    return linear(hidden_layer, output_layer_size)
 
 
 def _activate(tensor):

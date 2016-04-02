cdd582782a887031c8690ef443c6fa66054192a7
Raven Iqqe <raviqqe@gmail.com>
2016-04-02 15:49:03 +0900

    Add forward_documents and backward_documents properties to data.Data class

--- data.py
+++ data.py
@@ -18,12 +18,35 @@ class Data:
     return self._documents
 
   @property
+  def forward_documents(self):
+    if not hasattr(self, "_forward_documents"):
+      self._forward_documents \
+          = numpy.array([self._reverse_document(array)
+                         for array in self.backward_documents])
+    return self._forward_documents
+
+  @property
+  def backward_documents(self):
+    if not hasattr(self, "_backward_documents"):
+      self._backward_documents = numpy.fliplr(self._documents)
+    return self._backward_documents
+
+  @property
   def labels(self):
     return self._labels
 
   @property
   def size(self):
     return self._documents.shape[0]
+
+  @staticmethod
+  def _reverse_document(array):
+    if array[0] != 0:
+      return array[::-1]
+    padding_length = (array != 0).argmax(axis=0)
+    return numpy.concatenate((numpy.zeros((padding_length,),
+                                          dtype=numpy.int32),
+                             array[:(padding_length - 1):-1]))
 
 
 

2b8f06e7146f4a2d7d5029062e4cd3b5a2314860
Raven Iqqe <raviqqe@gmail.com>
2016-04-02 19:03:41 +0900

    Add a name scope to nn.embedding.embeddings_to_embedding()

--- nn/embedding/embeddings_to_embedding.py
+++ nn/embedding/embeddings_to_embedding.py
@@ -7,15 +7,16 @@ from ..util import static_shape, static_rank
 def embeddings_to_embedding(child_embeddings, output_embedding_size):
   assert static_rank(child_embeddings) == 3
 
-  embedding_size = static_shape(child_embeddings)[2]
-  rnn_cell = tf.nn.rnn_cell.GRUCell(output_embedding_size, embedding_size)
+  with tf.name_scope("embeddings_to_embedding"):
+    embedding_size = static_shape(child_embeddings)[2]
+    rnn_cell = tf.nn.rnn_cell.GRUCell(output_embedding_size, embedding_size)
 
-  state = rnn_cell.zero_state(tf.shape(child_embeddings)[0], tf.float32)
-  for iteration, child_embedding \
-      in enumerate(_split_child_embeddings(child_embeddings)):
-    if iteration != 0: tf.get_variable_scope().reuse_variables()
-    parent_embedding, state = rnn_cell(child_embedding, state)
-  return parent_embedding
+    state = rnn_cell.zero_state(tf.shape(child_embeddings)[0], tf.float32)
+    for iteration, child_embedding \
+        in enumerate(_split_child_embeddings(child_embeddings)):
+      if iteration != 0: tf.get_variable_scope().reuse_variables()
+      parent_embedding, state = rnn_cell(child_embedding, state)
+    return parent_embedding
 
 
 def _split_child_embeddings(child_embeddings):

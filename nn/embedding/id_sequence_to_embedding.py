375efd920a235dfc6a9efac1b967252a355c12ad
Raven Iqqe <raviqqe@gmail.com>
2016-05-02 16:50:14 +0900

    Add id_sequence_to_embedding function

---
+++ nn/embedding/id_sequence_to_embedding.py
@@ -0,0 +1,15 @@
+from .ids_to_embeddings import ids_to_embeddings
+from .embeddings_to_embedding import embeddings_to_embedding
+from ..util import static_rank
+
+
+def id_sequecne_to_embedding(child_id_sequence,
+                             *,
+                             output_embedding_size,
+                             context_vector_size):
+  assert static_rank(child_id_sequence) == 2
+
+  return embeddings_to_embedding(
+      ids_to_embeddings(child_id_sequence),
+      output_embedding_size=output_embedding_size,
+      context_vector_size=context_vector_size)

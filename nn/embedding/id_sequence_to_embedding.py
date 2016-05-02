bfc50caf2ad967fa930faf34c6cac6b20b7fd4a7
Raven Iqqe <raviqqe@gmail.com>
2016-05-02 16:55:46 +0900

    Fix missing argument of child embeddings

--- nn/embedding/id_sequence_to_embedding.py
+++ nn/embedding/id_sequence_to_embedding.py
@@ -4,12 +4,13 @@ from ..util import static_rank
 
 
 def id_sequecne_to_embedding(child_id_sequence,
+                             child_embeddings,
                              *,
                              output_embedding_size,
                              context_vector_size):
   assert static_rank(child_id_sequence) == 2
 
   return embeddings_to_embedding(
-      ids_to_embeddings(child_id_sequence),
+      ids_to_embeddings(child_id_sequence, child_embeddings),
       output_embedding_size=output_embedding_size,
       context_vector_size=context_vector_size)

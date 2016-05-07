a71fface9a9ce15fb2e125147457eb7ac11c8296
Raven Iqqe <raviqqe@gmail.com>
2016-05-07 17:44:54 +0900

    Use funcname_scope decorator

--- nn/models/char2word2sent2doc.py
+++ nn/models/char2word2sent2doc.py
@@ -5,10 +5,11 @@ from ..embedding import bidirectional_embeddings_to_embedding, \
                         embeddings
 from ..linear import linear
 from ..dropout import dropout
-from ..util import static_shape, static_rank
+from ..util import static_shape, static_rank, funcname_scope
 
 
 
+@funcname_scope
 def char2word2sent2doc(document,
                        *,
                        words,
@@ -21,38 +22,37 @@ def char2word2sent2doc(document,
                        hidden_layer_size,
                        output_layer_size,
                        context_vector_size):
-  with tf.name_scope("char2word2sent2doc"):
-    with tf.variable_scope("char_embedding"):
-      char_embeddings = embeddings(id_space_size=char_space_size,
-                                   embedding_size=char_embedding_size)
+  with tf.variable_scope("char_embedding"):
+    char_embeddings = embeddings(id_space_size=char_space_size,
+                                 embedding_size=char_embedding_size)
 
-    with tf.variable_scope("word_embedding"):
-      word_embeddings = bidirectional_id_sequence_to_embedding(
-          words,
-          char_embeddings,
-          output_embedding_size=word_embedding_size,
-          context_vector_size=context_vector_size,
-          dropout_prob=dropout_prob)
+  with tf.variable_scope("word_embedding"):
+    word_embeddings = bidirectional_id_sequence_to_embedding(
+        words,
+        char_embeddings,
+        output_embedding_size=word_embedding_size,
+        context_vector_size=context_vector_size,
+        dropout_prob=dropout_prob)
 
-    with tf.variable_scope("sentence_embedding"):
-      sentence_embeddings = bidirectional_id_sequence_to_embedding(
-          _flatten_document_to_sentences(document),
-          word_embeddings,
-          output_embedding_size=sentence_embedding_size,
-          context_vector_size=context_vector_size,
-          dropout_prob=dropout_prob)
+  with tf.variable_scope("sentence_embedding"):
+    sentence_embeddings = bidirectional_id_sequence_to_embedding(
+        _flatten_document_to_sentences(document),
+        word_embeddings,
+        output_embedding_size=sentence_embedding_size,
+        context_vector_size=context_vector_size,
+        dropout_prob=dropout_prob)
 
-    with tf.variable_scope("document_embedding"):
-      document_embedding = bidirectional_embeddings_to_embedding(
-          _restore_document_shape(sentence_embeddings, document),
-          output_embedding_size=document_embedding_size,
-          context_vector_size=context_vector_size,
-          dropout_prob=dropout_prob)
+  with tf.variable_scope("document_embedding"):
+    document_embedding = bidirectional_embeddings_to_embedding(
+        _restore_document_shape(sentence_embeddings, document),
+        output_embedding_size=document_embedding_size,
+        context_vector_size=context_vector_size,
+        dropout_prob=dropout_prob)
 
-    hidden_layer = dropout(_activate(linear(_activate(document_embedding),
-                                            hidden_layer_size)),
-                           dropout_prob)
-    return linear(hidden_layer, output_layer_size)
+  hidden_layer = dropout(_activate(linear(_activate(document_embedding),
+                                          hidden_layer_size)),
+                         dropout_prob)
+  return linear(hidden_layer, output_layer_size)
 
 
 def _concat(tensors):

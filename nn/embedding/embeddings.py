e4b69218bc780563335c8b7af8cd4488720837bc
Raven Iqqe <raviqqe@gmail.com>
2016-05-07 13:20:07 +0900

    Make embeddings()'s arguments keyword-only arguments

--- nn/embedding/embeddings.py
+++ nn/embedding/embeddings.py
@@ -4,5 +4,5 @@ from .. import var_init
 
 
 
-def embeddings(id_space_size, embedding_size):
+def embeddings(*, id_space_size, embedding_size):
   return tf.Variable(var_init.normal([id_space_size, embedding_size]))

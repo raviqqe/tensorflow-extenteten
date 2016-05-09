3abf549b98db015d59a6c14f065c06cfd4066263
Raven Iqqe <raviqqe@gmail.com>
2016-05-09 19:01:03 +0900

    s/dir/file/g with slmc module

---
+++ nn/slmc.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+from .util import static_shape, static_rank, funcname_scope
+
+
+
+@funcname_scope
+def loss(output_layer, true_label):
+  return tf.nn.sparse_softmax_cross_entropy_with_logits(output_layer,
+                                                        true_label)
+
+
+@funcname_scope
+def accuracy(output_layer: ("batch", "class"), true_label: ("batch",)):
+  assert static_rank(output_layer) == 2
+  assert static_rank(true_label) == 1
+  assert static_shape(output_layer)[0] == static_shape(true_label)[0]
+
+  return tf.reduce_mean(tf.to_float(tf.equal(predicted_label(output_layer),
+                                             true_label)))
+
+
+@funcname_scope
+def predicted_label(output_layer):
+  assert static_rank(output_layer) == 2
+  return tf.argmax(output_layer, 1)
--- nn/slmc/__init__.py
+++
@@ -1,3 +0,0 @@
-from .accuracy import accuracy
-from .loss import loss
-from .predicted_label import predicted_label

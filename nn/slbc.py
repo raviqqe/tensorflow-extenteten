c9c2f62ccac56aacde3eda9d5972684c451ddd18
Yota Toyama <raviqqe@gmail.com>
2016-05-11 19:52:51 +0900

    Make slbc compatible with both 1-dim and 2-dim output_layer arguments

--- nn/slbc.py
+++ nn/slbc.py
@@ -1,3 +1,4 @@
+import functools
 import tensorflow as tf
 
 from .util import static_shape, static_rank, funcname_scope
@@ -5,6 +6,7 @@ from .util import static_shape, static_rank, funcname_scope
 
 
 @funcname_scope
+@_squeeze_output_layer
 def classify(output_layer, true_label):
   assert static_rank(output_layer) == static_rank(true_label) == 1
   assert static_shape(output_layer) == static_shape(true_label)
@@ -16,6 +18,7 @@ def classify(output_layer, true_label):
 
 
 @funcname_scope
+@_squeeze_output_layer
 def loss(output_layer, true_label):
   assert static_rank(output_layer) == static_rank(true_label) == 1
   assert static_shape(output_layer) == static_shape(true_label)
@@ -26,6 +29,7 @@ def loss(output_layer, true_label):
 
 
 @funcname_scope
+@_squeeze_output_layer
 def accuracy(output_layer, true_label):
   assert static_rank(output_layer) == static_rank(true_label) == 1
   assert static_shape(output_layer) == static_shape(true_label)
@@ -36,6 +40,19 @@ def accuracy(output_layer, true_label):
 
 
 @funcname_scope
+@_squeeze_output_layer
 def predicted_label(output_layer):
   assert static_rank(output_layer) == 1
   return tf.sigmoid(output_layer) > 0.5
+
+
+def _squeeze_output_layer(func):
+  @functools.wraps(func)
+  def wrapper(output_layer, *args, **kwargs):
+    assert static_rank(output_layer) == 1 or static_rank(output_layer) == 2
+    return func(
+        tf.squeeze(output_layer, [1]) if static_rank(output_layer) == 2 else
+          output_layer,
+        *args,
+        **kwargs)
+  return wrapper

c81b680ee9ba6817e57ed14290ea07de9f0e6c1f
Yota Toyama <raviqqe@gmail.com>
2016-05-17 21:26:52 +0900

    Refactor embeddings_to_embedding function

--- nn/embedding/embeddings_to_embedding.py
+++ nn/embedding/embeddings_to_embedding.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 
-from ..util import static_rank, funcname_scope, dimension_indices
-from ..linear import linear
-from ..variable import variable
+from ..util import static_rank, funcname_scope
 from ..attention import attention_please
 from ..rnn import rnn
 
@@ -11,13 +9,9 @@ from ..rnn import rnn
 @funcname_scope
 def embeddings_to_embedding(child_embeddings,
                             *,
-                            output_embedding_size,
                             context_vector_size,
-                            dropout_prob):
+                            **rnn_hyper_params):
   assert static_rank(child_embeddings) == 3
 
-  cell_outputs = rnn(child_embeddings,
-                     output_embedding_size=output_embedding_size,
-                     dropout_prob=dropout_prob)
-
-  return attention_please(cell_outputs, context_vector_size)
+  return attention_please(rnn(child_embeddings, **rnn_hyper_params),
+                          context_vector_size)

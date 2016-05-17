cf4f62cae02fc0e677fdc3ac7a028b1ba57dacbb
Yota Toyama <raviqqe@gmail.com>
2016-05-17 14:49:37 +0900

    Disable word embedding cache
    This change must be tested and it should be made sure that it makes word embedding
    calculation fast and memory-efficient.

--- nn/models/char2word2sent2doc.py
+++ nn/models/char2word2sent2doc.py
@@ -22,6 +22,14 @@ def char2word2sent2doc(document,
                        hidden_layer_size,
                        output_layer_size,
                        context_vector_size):
+  """
+  The argument `document` is in the shape of
+  (batch size, #sentences / document, #words / sentence).
+  """
+
+  assert static_rank(document) == 3
+  assert static_rank(words) == 2
+
   with tf.variable_scope("char_embedding"):
     char_embeddings = embeddings(id_space_size=char_space_size,
                                  embedding_size=char_embedding_size,
@@ -29,16 +37,15 @@ def char2word2sent2doc(document,
 
   with tf.variable_scope("word_embedding"):
     word_embeddings = bidirectional_id_sequence_to_embedding(
-        words,
+        tf.gather(words, _flatten_document_to_words(document)),
         char_embeddings,
         output_embedding_size=word_embedding_size,
         context_vector_size=context_vector_size,
         dropout_prob=dropout_prob)
 
   with tf.variable_scope("sentence_embedding"):
-    sentence_embeddings = bidirectional_id_sequence_to_embedding(
-        _flatten_document_to_sentences(document),
-        word_embeddings,
+    sentence_embeddings = bidirectional_embeddings_to_embedding(
+        _restore_sentence_shape(word_embeddings, document),
         output_embedding_size=sentence_embedding_size,
         context_vector_size=context_vector_size,
         dropout_prob=dropout_prob)
@@ -65,8 +72,14 @@ def _restore_document_shape(sentences, document):
                                     + static_shape(sentences)[1:])
 
 
-def _flatten_document_to_sentences(document):
-  return tf.reshape(document, [-1] + static_shape(document)[2:])
+def _restore_sentence_shape(words, document):
+  return tf.reshape(
+      words,
+      [-1, static_shape(document)[2]] + static_shape(words)[1:])
+
+
+def _flatten_document_to_words(document):
+  return tf.reshape(document, [-1])
 
 
 def _activate(tensor):

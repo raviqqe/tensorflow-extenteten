f37b2cef0bebedf71c31abe0b6a966606e2ca55c
Yota Toyama <raviqqe@gmail.com>
2016-05-17 21:23:51 +0900

    Fix attention of bidirectional_embeddings_to_embedding function

--- nn/embedding/bidirectional_embeddings_to_embedding.py
+++ nn/embedding/bidirectional_embeddings_to_embedding.py
@@ -2,29 +2,38 @@ import functools
 import tensorflow as tf
 
 from ..util import static_shape, static_rank, funcname_scope
+from ..rnn import rnn
 from .embeddings_to_embedding import embeddings_to_embedding
+from ..attention import attention_please
 
 
 
 @funcname_scope
 def bidirectional_embeddings_to_embedding(child_embeddings,
-                                          **kwargs):
-  child_embeddings_to_embedding = functools.partial(
-      embeddings_to_embedding,
-      **kwargs)
+                                          *,
+                                          context_vector_size,
+                                          output_embedding_size,
+                                          **rnn_hyper_params):
+  assert output_embedding_size % 2 == 0
+
+  rnn_ = functools.partial(rnn,
+                           output_embedding_size=output_embedding_size // 2,
+                           **rnn_hyper_params)
 
   with tf.variable_scope("forward"):
-    forward_embedding = child_embeddings_to_embedding(child_embeddings)
+    forward_outputs = rnn_(child_embeddings)
 
   with tf.variable_scope("backward"):
-    backward_embedding = child_embeddings_to_embedding(
-        _reverse_embedding_sequence(child_embeddings))
+    backward_outputs = rnn_(_reverse_embedding_sequence(child_embeddings))
 
-  return _concat_embeddings(forward_embedding, backward_embedding)
+  return attention_please(
+      _concat_rnn_outputs(forward_outputs, backward_outputs),
+      context_vector_size=context_vector_size)
 
 
-def _concat_embeddings(*embeddings):
-  return tf.concat(1, list(embeddings))
+def _concat_rnn_outputs(*rnn_outputs):
+  assert all(static_rank(rnn_output) == 3 for rnn_output in rnn_outputs)
+  return tf.concat(2, list(rnn_outputs))
 
 
 def _reverse_embedding_sequence(embedding_sequence):

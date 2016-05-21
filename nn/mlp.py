3982d573a91641c48b143c044abebb137b157a3a
Yota Toyama <raviqqe@gmail.com>
2016-05-21 22:36:09 +0900

    Don't drop last layer of mlp

--- nn/mlp.py
+++ nn/mlp.py
@@ -12,15 +12,14 @@ def mlp(x, *, layer_sizes, dropout_prob, activate=tf.nn.elu):
   assert static_rank(x) == 2
   assert all(isinstance(size, int) for size in layer_sizes)
 
-  fully_connected_ = functools.partial(fully_connected,
-                                       dropout_prob=dropout_prob)
-
   def activated_fully_connected(x, output_layer_size):
-    return fully_connected_(
+    return fully_connected(
         x,
         output_layer_size=output_layer_size,
-        activate=activate)
+        activate=activate,
+        dropout_prob=dropout_prob)
 
-  return fully_connected_(
+  return fully_connected(
       functools.reduce(activated_fully_connected, layer_sizes[:-1], x),
-      output_layer_size=layer_sizes[-1])
+      output_layer_size=layer_sizes[-1],
+      dropout_prob=0)

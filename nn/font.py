d5bd22ab707ddd2dbd6f37f203c6bf60925c9535
Yota Toyama <raviqqe@gmail.com>
2016-05-23 21:09:54 +0900

    Shrink character embedding size

--- nn/font.py
+++ nn/font.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 
 from .conv import lenet
+from .layer import fully_connected
 from .util import static_rank, static_shape, funcname_scope
 
 
 
 @funcname_scope
-def font2char(fonts):
+def font2char(fonts, *, dropout_prob, char_embedding_size):
   assert static_rank(fonts) == 3
 
-  return tf.reshape(lenet(tf.expand_dims(fonts, -1)),
-                    [static_shape(fonts)[0], -1])
+  return fully_connected(tf.reshape(lenet(tf.expand_dims(fonts, -1)),
+                                    [static_shape(fonts)[0], -1]),
+                         dropout_prob=dropout_prob,
+                         output_layer_size=char_embedding_size,
+                         activate=None)

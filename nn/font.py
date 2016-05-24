8f50e8fc7008b55dfe852538d6bf19ab50f1453a
Yota Toyama <raviqqe@gmail.com>
2016-05-24 12:21:59 +0900

    Add font image summary

--- nn/font.py
+++ nn/font.py
@@ -10,8 +10,11 @@ from .util import static_rank, static_shape, funcname_scope
 def font2char(fonts, *, dropout_prob, char_embedding_size):
   assert static_rank(fonts) == 3
 
-  return fully_connected(tf.reshape(lenet(tf.expand_dims(fonts, -1)),
-                                    [static_shape(fonts)[0], -1]),
-                         dropout_prob=dropout_prob,
-                         output_layer_size=char_embedding_size,
-                         activate=None)
+  images = tf.expand_dims(fonts, -1)
+  tf.image_summary("fonts", images, max_images=256)
+
+  return fully_connected(
+      tf.reshape(lenet(images), [static_shape(fonts)[0], -1]),
+      dropout_prob=dropout_prob,
+      output_layer_size=char_embedding_size,
+      activate=None)

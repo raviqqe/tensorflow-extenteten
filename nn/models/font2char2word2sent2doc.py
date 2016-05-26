550ba59a7fdedfaac2469d55630574b01e593fcd
Yota Toyama <raviqqe@gmail.com>
2016-05-26 10:04:46 +0900

    Remove extra variable scope

--- nn/models/font2char2word2sent2doc.py
+++ nn/models/font2char2word2sent2doc.py
@@ -18,13 +18,12 @@ def font2char2word2sent2doc(document,
   assert static_rank(words) == 2
   assert static_rank(fonts) == 3
 
-  with tf.variable_scope("char_embedding"):
-    char_embeddings = font2char(fonts,
-                                dropout_prob=dropout_prob,
-                                char_embedding_size=char_embedding_size)
 
-  return ar2word2sent2doc(document,
-                          words=words,
-                          char_embeddings=char_embeddings,
-                          dropout_prob=dropout_prob,
-                          **ar2word2sent2doc_hyper_params)
+  return ar2word2sent2doc(
+      document,
+      words=words,
+      char_embeddings=font2char(fonts,
+                                dropout_prob=dropout_prob,
+                                char_embedding_size=char_embedding_size),
+      dropout_prob=dropout_prob,
+      **ar2word2sent2doc_hyper_params)

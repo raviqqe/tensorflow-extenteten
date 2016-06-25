ac30108347f6ee73acd215c3ad24d10fb29da7f5
Yota Toyama <raviqqe@gmail.com>
2016-06-25 14:44:16 +0900

    Import a function with name

--- nn/embedding/bidirectional_id_sequence_to_embedding.py
+++ nn/embedding/bidirectional_id_sequence_to_embedding.py
@@ -2,7 +2,7 @@ from .ids_to_embeddings import ids_to_embeddings
 from .bidirectional_embeddings_to_embedding \
     import bidirectional_embeddings_to_embedding
 from ..util import static_rank, funcname_scope
-from ..dynamic_length import *
+from ..dynamic_length import id_sequence_to_length
 
 
 

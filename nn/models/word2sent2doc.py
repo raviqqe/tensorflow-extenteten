e4a990ad07f3993d00d69f64680668eb00a3b77c
Yota Toyama <raviqqe@gmail.com>
2016-06-25 17:57:03 +0900

    Adjust word2sent2doc model for new interface of rd2sent2doc

--- nn/models/word2sent2doc.py
+++ nn/models/word2sent2doc.py
@@ -15,10 +15,8 @@ def word2sent2doc(document,
   assert static_rank(document) == 3
 
   with tf.variable_scope("word_embeddings"):
-    word_embeddings = tf.gather(
-        embeddings(id_space_size=word_space_size,
-                   embedding_size=word_embedding_size,
-                   name="word_embeddings"),
-        document)
+    word_embeddings = embeddings(id_space_size=word_space_size,
+                                 embedding_size=word_embedding_size,
+                                 name="word_embeddings")
 
-  return rd2sent2doc(word_embeddings, **rd2sent2doc_hyperparams)
+  return rd2sent2doc(document, word_embeddings, **rd2sent2doc_hyperparams)

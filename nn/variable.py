9661ac1d420f1a0c2f79600a714a5265d0bf74d6
Yota Toyama <raviqqe@gmail.com>
2016-07-15 21:15:49 +0900

    Use Variable

--- nn/variable.py
+++ nn/variable.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 
 
 def variable(shape, name=None):
-  return tf.get_variable(
-      name,
-      shape,
-      initializer=(tf.contrib.layers.xavier_initializer()
-                   if len(shape) == 2 else
-                   tf.random_normal_initializer(stddev=0.1)))
+  return tf.Variable(
+      (tf.contrib.layers.xavier_initializer()
+       if len(shape) == 2 else
+       tf.random_normal_initializer(stddev=0.1))(shape),
+      name=name)

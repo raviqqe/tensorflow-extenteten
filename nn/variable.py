857075c797ab86008ad50f1016896d3ccb675a9d
Yota Toyama <raviqqe@gmail.com>
2016-07-15 20:52:04 +0900

    Use Xavier initialization

--- nn/variable.py
+++ nn/variable.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 
 
 def variable(shape, name=None):
-  return tf.Variable(_normal(shape), name=name)
-
-
-def _normal(shape):
-  return tf.truncated_normal(shape, stddev=0.1)
+  return tf.get_variable(
+      name,
+      shape,
+      initializer=(tf.contrib.layers.xavier_initializer()
+                   if len(shape) == 2 else
+                   tf.random_normal_initializer(stddev=0.1)))

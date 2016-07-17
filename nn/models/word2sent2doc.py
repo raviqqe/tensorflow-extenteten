f527f3b7c8909681f2ed3707fd37d40c094d665c
Yota Toyama <raviqqe@gmail.com>
2016-07-17 18:40:32 +0900

    Use save_memory implementation of rd2sent2doc for word2sent2doc model

--- nn/models/word2sent2doc.py
+++ nn/models/word2sent2doc.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from ..embedding import embeddings
-from ..util import static_rank, funcname_scope
+from ..util import static_rank, funcname_scope, flatten
 from .rd2sent2doc import rd2sent2doc
 
 
@@ -15,8 +15,13 @@ def word2sent2doc(document,
   assert static_rank(document) == 3
 
   with tf.variable_scope("word_embeddings"):
-    word_embeddings = embeddings(id_space_size=word_space_size,
-                                 embedding_size=word_embedding_size,
-                                 name="word_embeddings")
+    word_embeddings = tf.gather(
+        embeddings(id_space_size=word_space_size,
+                   embedding_size=word_embedding_size,
+                   name="word_embeddings"),
+        flatten(document))
 
-  return rd2sent2doc(document, word_embeddings, **rd2sent2doc_hyperparams)
+  return rd2sent2doc(document,
+                     word_embeddings,
+                     save_memory=True,
+                     **rd2sent2doc_hyperparams)

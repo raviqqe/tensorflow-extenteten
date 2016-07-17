8e17f79503a6e2ea09561d4c1bacc0acfa978f2f
Yota Toyama <raviqqe@gmail.com>
2016-07-17 18:57:51 +0900

    Return summary

--- nn/summary.py
+++ nn/summary.py
@@ -9,12 +9,15 @@ def summarize(variable, name=None):
   summary_name = name or variable.name
 
   mean = tf.reduce_mean(variable)
-  tf.scalar_summary("mean/" + summary_name, mean)
-  tf.scalar_summary("stddev/" + summary_name,
-                    tf.sqrt(tf.reduce_sum(tf.square(variable - mean))))
-  tf.scalar_summary("max/" + summary_name, tf.reduce_max(variable))
-  tf.scalar_summary("min/" + summary_name, tf.reduce_min(variable))
-  tf.histogram_summary(summary_name, variable)
+
+  return tf.merge_summary([
+    tf.scalar_summary("mean/" + summary_name, mean),
+    tf.scalar_summary("stddev/" + summary_name,
+                      tf.sqrt(tf.reduce_sum(tf.square(variable - mean)))),
+    tf.scalar_summary("max/" + summary_name, tf.reduce_max(variable)),
+    tf.scalar_summary("min/" + summary_name, tf.reduce_min(variable)),
+    tf.histogram_summary(summary_name, variable),
+  ])
 
 
 @funcname_scope

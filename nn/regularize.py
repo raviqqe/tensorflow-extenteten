36d2c3c8cc04f94b7206d6db6fa7e1dd7c192806
Yota Toyama <raviqqe@gmail.com>
2016-07-20 15:10:54 +0900

    Refactor regularize module

--- nn/regularize.py
+++ nn/regularize.py
@@ -6,6 +6,5 @@ from .util import funcname_scope
 
 @funcname_scope
 def regularize_with_l2_loss(scale):
-  return scale * tf.reduce_sum(tf.pack([
-      tf.nn.l2_loss(weight)
-      for weight in tf.get_collection(tf.GraphKeys.WEIGHTS)]))
+  return tf.contrib.layers.apply_regularization(
+      tf.contrib.layers.l2_regularizer(scale))

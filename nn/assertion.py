7cc3e1c59aa443cb662282814dc1d436b742011d
Yota Toyama <raviqqe@gmail.com>
2016-07-20 11:21:22 +0900

    Add multi_conv()

--- nn/assertion.py
+++ nn/assertion.py
@@ -1,2 +1,11 @@
-def check_natural_num(num):
-  return isinstance(num, int) and num > 0
+from .util import is_int
+
+
+def is_natural_num(num):
+  return is_int(num) and num > 0
+
+
+def is_natural_num_list(num_list, length=None):
+  return isinstance(num_list, list) and \
+         all(is_natural_num(num) for num in num_list) and \
+         (length == None or len(num_list) == length)
--- nn/conv.py
+++ nn/conv.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from .util import funcname_scope, static_rank, static_shape
 from .variable import variable
-from .assertion import check_natural_num
+from .assertion import is_natural_num, is_natural_num_list
 
 
 
@@ -13,11 +13,27 @@ def multi_conv_and_pool(x,
                         nums_of_channels=[20, 50],
                         conv_kernel_shape=[5, 5],
                         pool_kernel_shape=[2, 2]):
+  assert is_natural_num_list(nums_of_channels)
+  assert _is_kernel_shape(conv_kernel_shape)
+  assert _is_kernel_shape(pool_kernel_shape)
+
   def layer(x, num_of_channels):
-    c = tf.tanh(conv2d(x,
-                       kernel_shape=conv_kernel_shape,
-                       num_of_channels=num_of_channels))
-    return max_pool(c, kernel_shape=pool_kernel_shape)
+    return max_pool(tf.tanh(conv2d(x, conv_kernel_shape, num_of_channels)),
+                    kernel_shape=pool_kernel_shape)
+
+  return functools.reduce(layer, nums_of_channels, x)
+
+
+@funcname_scope
+def multi_conv(x,
+               *,
+               nums_of_channels,
+               kernel_shape):
+  assert is_natural_num_list(nums_of_channels)
+  assert _is_kernel_shape(kernel_shape)
+
+  def layer(x, num_of_channels):
+    return tf.tanh(conv2d(x, kernel_shape, num_of_channels))
 
   return functools.reduce(layer, nums_of_channels, x)
 
@@ -25,8 +41,8 @@ def multi_conv_and_pool(x,
 @funcname_scope
 def conv2d(x, kernel_shape, num_of_channels):
   assert static_rank(x) == 4
-  assert _check_kernel_shape(kernel_shape)
-  assert check_natural_num(num_of_channels)
+  assert _is_kernel_shape(kernel_shape)
+  assert is_natural_num(num_of_channels)
 
   return tf.nn.conv2d(
       x,
@@ -38,12 +54,11 @@ def conv2d(x, kernel_shape, num_of_channels):
 
 @funcname_scope
 def max_pool(x, kernel_shape):
-  assert _check_kernel_shape(kernel_shape)
+  assert _is_kernel_shape(kernel_shape)
 
   strides = [1] + list(kernel_shape) + [1]
   return tf.nn.max_pool(x, ksize=strides, strides=strides, padding="SAME")
 
 
-def _check_kernel_shape(kernel_shape):
-  return len(kernel_shape) == 2 and \
-         all(check_natural_num(size) for size in kernel_shape)
+def _is_kernel_shape(shape):
+  return is_natural_num_list(shape, 2)

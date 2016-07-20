7024da843ed015f72426089857762d86e4f36399
Yota Toyama <raviqqe@gmail.com>
2016-07-20 10:50:00 +0900

    Make lenet's hyperparameters arguments

--- nn/conv.py
+++ nn/conv.py
@@ -8,28 +8,29 @@ from .assertion import check_natural_num
 
 
 @funcname_scope
-def lenet(x):
-  conv = functools.partial(conv2d, kernel_shape=[5, 5])
-  pool = functools.partial(max_pool, kernel_shape=[2, 2])
-  activate = tf.tanh
+def lenet(x,
+          nums_of_channels=[20, 50],
+          conv_kernel_shape=[5, 5],
+          pool_kernel_shape=[2, 2]):
+  def layer(x, num_of_channels):
+    c = tf.tanh(conv2d(x,
+                       kernel_shape=conv_kernel_shape,
+                       num_of_channels=num_of_channels))
+    return max_pool(c, kernel_shape=pool_kernel_shape)
 
-  h = activate(conv(x, num_of_output_channels=20))
-  h = pool(h)
-  h = activate(conv(h, num_of_output_channels=50))
-  return pool(h)
+  return functools.reduce(layer, nums_of_channels, x)
 
 
 @funcname_scope
-def conv2d(x, kernel_shape, num_of_output_channels):
+def conv2d(x, kernel_shape, num_of_channels):
   assert static_rank(x) == 4
   assert _check_kernel_shape(kernel_shape)
-  assert check_natural_num(num_of_output_channels)
+  assert check_natural_num(num_of_channels)
 
   return tf.nn.conv2d(
       x,
-      variable(
-        list(kernel_shape) + [static_shape(x)[-1], num_of_output_channels],
-        name="kernel"),
+      variable(list(kernel_shape) + [static_shape(x)[-1], num_of_channels],
+               name="kernel"),
       strides=[1, 1, 1, 1],
       padding="SAME")
 

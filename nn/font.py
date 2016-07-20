7f92aa0db1960f1d15858222dfaa90503e9f911f
Yota Toyama <raviqqe@gmail.com>
2016-07-20 19:44:51 +0900

    Use image_summary

--- nn/font.py
+++ nn/font.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from .conv import multi_conv
 from .layer import fully_connected
 from .random import sample_crop
+from .summary import image_summary
 from .util import static_rank, static_shape, funcname_scope
 
 
@@ -12,7 +13,7 @@ def font2char(fonts, *, dropout_prob, char_embedding_size, **conv_hyperparams):
   assert static_rank(fonts) == 3
 
   images = tf.expand_dims(fonts, -1)
-  tf.image_summary("fonts", sample_crop(images, 8))
+  image_summary(sample_crop(images, 8))
 
   return fully_connected(
       tf.reshape(multi_conv(images, **conv_hyperparams),

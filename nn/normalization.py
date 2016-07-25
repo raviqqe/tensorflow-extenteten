Fix implementation of layer normalization

nn/normalization.py
@@ -1,14 +1,20 @@
+import functools
 import tensorflow as tf
 
-from .util import funcname_scope, static_rank, dimension_indices
+from .util import funcname_scope, static_shape, static_rank, dimension_indices
 from .variable import variable
 
 
 
 @funcname_scope
-def layer_normalization(x: ("batch", ...)):
+def layer_normalization(x: ("batch", ...), add_bias=True):
   assert static_rank(x) >= 2
-  return (x - _batchwise_mean(x)) / _stddev(x) * variable([], "stddev")
+  param_shape = [1, *static_shape(x)[1:]]
+  a = (x - _batchwise_mean(x)) \
+      / _stddev(x) * variable(tf.ones(param_shape), "gain")
+  bias = variable(tf.zeros(param_shape), "bias")
+  tf.add_to_collection(tf.GraphKeys.BIASES, bias)
+  return a + bias if add_bias else a
 
 
 @funcname_scope
@@ -18,6 +24,6 @@ def _batchwise_mean(x):
 
 @funcname_scope
 def _stddev(x):
-  return tf.sqrt(tf.reduce_sum(tf.square(x - _batchwise_mean(x)),
-                               dimension_indices(x, 1),
-                               keep_dims=True))
+  return tf.sqrt(tf.reduce_mean(tf.square(x - _batchwise_mean(x)),
+                                dimension_indices(x, 1),
+                                keep_dims=True))

nn/variable.py
@@ -1,10 +1,19 @@
+import functools
 import tensorflow as tf
 
+from .assertion import is_natural_num_sequence
 
 
-def variable(shape, name=None):
-  return tf.Variable(
-      (tf.contrib.layers.xavier_initializer()
-       if len(shape) == 2 else
-       tf.truncated_normal_initializer(stddev=0.1))(shape),
-      name=name)
+
+def variable(shape_or_initial, name=None):
+  create_variable = functools.partial(tf.Variable, name=name)
+
+  if is_natural_num_sequence(shape_or_initial):
+    shape = shape_or_initial
+    return create_variable(
+        (tf.contrib.layers.xavier_initializer()
+         if len(shape) == 2 else
+         tf.truncated_normal_initializer(stddev=0.1))(shape))
+
+  initial = shape_or_initial
+  return create_variable(initial)
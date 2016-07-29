c93106165690ee56724f66a4fd59b3e31976394e
Yota Toyama <raviqqe@gmail.com>
2016-07-29 10:06:19 +0900

    Implement invertible.InvertibleNetwork class

--- nn/invertible.py
+++ nn/invertible.py
@@ -1,10 +1,33 @@
 import abc
+import functools
 
+
+
+_FORWARD = "forward"
+_BACKWARD = "backward"
 
 
 class InvertibleLayer(abc.ABC):
-  def forward(self, *args, **kwargs):
+  def forward(self, *args):
     return NotImplemented
 
-  def backward(self, *args, **kwargs):
+  def backward(self, *args):
     return NotImplemented
+
+
+class InvertibleNetwork(InvertibleLayer):
+  def __init__(self, *layers):
+    assert all(isinstance(layer, InvertibleLayer) for layer in layers)
+    self._layers = layers
+
+  def forward(self, *args):
+    return _reduce_layers(_FORWARD, *args, **kwargs)
+
+  def backward(self, *args):
+    return _reduce_layers(_BACKWARD, *args, **kwargs)
+
+  def _reduce_layers(self, method, *args):
+    assert method in {_FORWARD, _BACKWARD}
+    return functools.reduce(lambda args, layer: getattr(layer, method)(*args),
+                            self._layers,
+                            args)

590bc9579c1c549cf8b9517b7082666fdceee44b
Yota Toyama <raviqqe@gmail.com>
2016-11-04 06:38:13 +0900

    Create main module

--- nn/flags.py
+++ nn/flags.py
@@ -3,4 +3,3 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_float("dropout-prob", 0, "Dropout probability")
-tf.app.flags.DEFINE_int("n-epochs", 1, "Number of epochs")
---
+++ nn/main.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+from flags import FLAGS
+
+
+
+list_of_hosts = "Comma-separated list of hostname:port pairs"
+tf.app.flags.DEFINE_string("ps-hosts", None, list_of_hosts)
+tf.app.flags.DEFINE_string("worker-hosts", None, list_of_hosts)
+
+tf.app.flags.DEFINE_string("job-name", None, "'ps' or 'worker'")
+tf.app.flags.DEFINE_integer("task-index", None, "Task index within the job")
+
+tf.app.flags.DEFINE_int(
+    "file-glob", None, "File path glob to search data files")
+tf.app.flags.DEFINE_string(
+    "log-dir", None, "Log directory containing checkpoint and event files")
+tf.app.flags.DEFINE_int("n-epochs", None, "Number of epochs")
+
+
+
+def main(model):
+  def run(_):
+    cluster = tf.train.ClusterSpec({
+      "ps": FLAGS.ps_hosts.split(","),
+      "worker": FLAGS.worker_hosts.split(","),
+    })
+
+    server = tf.train.Server(cluster,
+                             job_name=FLAGS.job_name,
+                             task_index=FLAGS.task_index)
+
+    if FLAGS.job_name == "ps":
+      server.join()
+    elif FLAGS.job_name == "worker":
+      with tf.device(tf.train.replica_device_setter(
+          worker_device="/job:worker/task:{}".format(FLAGS.task_index),
+          cluster=cluster)):
+        global_step = tf.Variable(0, trainable=False, name="global_step")
+
+        train_op = tf.train.AdamOptimizer().minimize(
+            model(Queue(FLAGS.file_glob).dequeue()), # TODO: Calculate loss properly
+            global_step=global_step)
+
+        saver = tf.train.Saver()
+        summary_op = tf.summary.merge_all()
+        init_op = tf.initialize_all_variables()
+
+      sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
+                               logdir=FLAGS.log_dir,
+                               init_op=init_op,
+                               summary_op=summary_op,
+                               saver=saver,
+                               global_step=global_step)
+
+      with sv.managed_session(server.target) as sess, sess.as_default():
+        step = global_step.eval()
+        while not sv.should_stop() and step < FLAGS.n_epochs: # TODO: n_epochs != n_steps
+          _, step = sess.run([train_op, global_step])
+
+      sv.stop()
+
+  tf.app.run(run)

471aaf167d8e803f1b8ae95872a8d97cd36c0036
Yota Toyama <raviqqe@gmail.com>
2016-11-04 03:18:23 +0900

    Remove hp variables in flags module

--- nn/flags.py
+++ nn/flags.py
@@ -3,8 +3,4 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_float("dropout-prob", 0, "Dropout probability")
-DROPOUT_PROB = FLAGS.dropout_prob
-DROPOUT_KEEP_PROB = 1 - DROPOUT_PROB
-
 tf.app.flags.DEFINE_int("n-epochs", 1, "Number of epochs")
-N_EPOCHS = FLAGS.n_epochs

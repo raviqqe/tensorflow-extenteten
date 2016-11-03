ef98069dd8ed06289bd6a05d146e5d1283d0e936
Yota Toyama <raviqqe@gmail.com>
2016-11-04 00:18:55 +0900

    Define n_epochs

--- nn/flags.py
+++ nn/flags.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_float("dropout-prob", 0, "Dropout probability")
 DROPOUT_PROB = FLAGS.dropout_prob
 DROPOUT_KEEP_PROB = 1 - DROPOUT_PROB
+
+tf.app.flags.DEFINE_int("n-epochs", 1, "Number of epochs")
+N_EPOCHS = FLAGS.n_epochs

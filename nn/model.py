0307f9adfb45be5efbe6f69a6a61c4567991f43c
Yota Toyama <raviqqe@gmail.com>
2016-11-06 06:19:44 +0900

    Add model module

---
+++ nn/model.py
@@ -0,0 +1,17 @@
+import abc
+import tensorflow as tf
+
+
+
+class Model(metaclass=abc.ABCMeta):
+  @abc.astractmethod
+  def __init__(self, **hyperparameters_and_initial_parameters):
+    return NotImplemented
+
+  @abc.astractmethod
+  def train(self, *input_tensors) -> tf.Tensor: # scalar loss
+    return NotImplemented
+
+  @abc.astractmethod
+  def test(self, *input_tensors) -> tf.Tensor: # labels
+    return NotImplemented

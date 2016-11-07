eb887c072b0f446da29807c3917d1de2c689a01f
Yota Toyama <raviqqe@gmail.com>
2016-11-07 10:38:11 +0900

    Define batch-size

--- nn/flags.py
+++ nn/flags.py
@@ -2,4 +2,5 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_float("dropout-prob", 0, "Dropout probability")
+tf.app.flags.DEFINE_string("batch-size", 32, "")
+tf.app.flags.DEFINE_float("dropout-prob", 0, "")

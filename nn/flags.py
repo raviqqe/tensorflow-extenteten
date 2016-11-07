853a32991b7ef0978a0a2b2295e2763178dc49fa
Yota Toyama <raviqqe@gmail.com>
2016-11-07 12:42:39 +0900

    Add flags

--- nn/flags.py
+++ nn/flags.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string("batch-size", 32, "")
+tf.app.flags.DEFINE_string("batch-size", 64, "")
 tf.app.flags.DEFINE_float("dropout-prob", 0, "")
 tf.app.flags.DEFINE_string("word-file", None, "")
+tf.app.flags.DEFINE_integer("num-threads-per-queue", 2, "")
+tf.app.flags.DEFINE_integer("queue-capacity", 2, "")

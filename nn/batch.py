06403e96fed40a00a332fada2ea661b5dfbb8a97
Yota Toyama <raviqqe@gmail.com>
2016-11-09 12:54:27 +0900

    Add batch max min and sum

--- nn/batch.py
+++ nn/batch.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
-from .util import funcname_scope, static_rank
+from .util import funcname_scope, static_rank, dimension_indices
+from .math import vec_to_mat
 
 
 
@@ -16,4 +17,22 @@ def mat_vec_mul(matrix, vector):
   assert static_rank(matrix) == 3
   assert static_rank(vector) == 2
 
-  return tf.squeeze(tf.batch_matmul(matrix, tf.expand_dims(vector, [2])), [2])
+  return tf.squeeze(tf.batch_matmul(matrix, vec_to_mat(vector)), [2])
+
+
+@funcname_scope
+def max(x, keep_dims=False, name=None):
+  return tf.reduce_max(x, dimension_indices(x, 1),
+                       keep_dims=keep_dims, name=name)
+
+
+@funcname_scope
+def min(x, keep_dims=False, name=None):
+  return tf.reduce_min(x, dimension_indices(x, 1),
+                       keep_dims=keep_dims, name=name)
+
+
+@funcname_scope
+def sum(x, keep_dims=False, name=None):
+  return tf.reduce_sum(x, dimension_indices(x, 1),
+                       keep_dims=keep_dims, name=name)

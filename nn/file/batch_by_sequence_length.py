271eacc8aa94b50b3df9bec5cc4dd91bc557399d
Yota Toyama <raviqqe@gmail.com>
2016-11-10 01:04:55 +0900

    Add batch_by_sequence_length function

---
+++ nn/file/batch_by_sequence_length.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+from ..flags import FLAGS
+
+
+
+@funcname_scope
+def batch_by_sequence_length(tensors, length_fn=(lambda x: tf.shape(x[1])[0])):
+  return tf.contrib.training.bucket_by_sequence_length(
+      length_fn(tensors), # first tensor is key
+      list(tensors),
+      FLAGS.batch_size,
+      [int(num) for num in FLAGS.length_boundaries.split(",")],
+      num_threads=FLAGS.num_threads_per_queue,
+      capacity=FLAGS.queue_capacity,
+      dynamic_pad=True)[1]

ed1f03f167ef17e6c9ce54bb55c0f39a1dfb6682
Yota Toyama <raviqqe@gmail.com>
2016-11-10 13:58:04 +0900

    Add Model class

---
+++ nn/model.py
@@ -0,0 +1,16 @@
+import abc
+
+
+
+class Model(metaclass=abc.ABCMeta):
+  @abc.abstractproperty
+  def train_op(self):
+    return NotImplemented
+
+  @abc.abstractproperty
+  def labels(self):
+    return NotImplemented
+
+  @property
+  def metrics(self):
+    return {}

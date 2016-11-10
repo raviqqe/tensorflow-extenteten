386a2c5ec7763ba427e4747efe98e6b2bb75ca63
Yota Toyama <raviqqe@gmail.com>
2016-11-11 00:09:00 +0900

    Re-export AttentionSumReader

--- nn/models/__init__.py
+++ nn/models/__init__.py
@@ -2,3 +2,4 @@ from .char2doc import char2doc
 from .char2word2sent2doc import char2word2sent2doc
 from .word2sent2doc import word2sent2doc
 from .font2char2word2sent2doc import font2char2word2sent2doc
+from .attention_sum_reader import AttentionSumReader

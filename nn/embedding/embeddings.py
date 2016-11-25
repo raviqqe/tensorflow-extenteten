cd5c56583c84b2b0fd05d743578193b7b681151c
Yota Toyama <raviqqe@gmail.com>
2016-11-26 04:22:24 +0900

    Add func_scope to embedding functions

--- nn/embedding/embeddings.py
+++ nn/embedding/embeddings.py
@@ -2,13 +2,16 @@ import tensorflow as tf
 
 from ..flags import FLAGS
 from ..variable import variable
+from ..util import func_scope
 
 
 
+@func_scope()
 def embeddings(*, id_space_size, embedding_size, name=None):
   return variable([id_space_size, embedding_size], name=name)
 
 
+@func_scope()
 def word_embeddings(name="word_embeddings"):
   if FLAGS.word_embeddings is None:
     return embeddings(id_space_size=FLAGS.word_space_size,

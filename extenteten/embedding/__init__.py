b2b9105101048e7b5480f6460bb56f5e2a467023
Yota Toyama <raviqqe@gmail.com>
2017-01-06 00:21:16 +0900

    Format embedding.__init__

--- extenteten/embedding/__init__.py
+++ extenteten/embedding/__init__.py
@@ -1,6 +1,6 @@
 from .unidirectional import embeddings_to_embedding, id_sequence_to_embedding
-from .bidirectional import bidirectional_embeddings_to_embedding, \
-    bidirectional_id_sequence_to_embedding, \
-    bidirectional_id_sequence_to_embeddings
+from .bidirectional import (bidirectional_embeddings_to_embedding,
+                            bidirectional_id_sequence_to_embedding,
+                            bidirectional_id_sequence_to_embeddings)
 from .embeddings import embeddings
 from .ids_to_embeddings import ids_to_embeddings

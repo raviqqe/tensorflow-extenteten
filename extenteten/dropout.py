6bcf7bac118b0464af8894e11cbb3bec69f3e96b
Yota Toyama <raviqqe@gmail.com>
2017-01-05 16:19:49 +0900

    Pass mode to dropout

--- extenteten/dropout.py
+++ extenteten/dropout.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
 
-def dropout(x, dropout_prob):
-    return tf.nn.dropout(x, 1 - dropout_prob)
+def dropout(x, dropout_prob, mode):
+    return tf.nn.dropout(
+        x,
+        1 - dropout_prob if mode == tf.contrib.learn.ModeKeys.TRAIN else 0)

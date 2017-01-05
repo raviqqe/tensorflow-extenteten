132a18d3efe6b58ed47658e61e3488a2d5cd8154
Yota Toyama <raviqqe@gmail.com>
2017-01-05 21:06:35 +0900

    Rename files related to classification

--- extenteten/mlmc.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-from .multi_label import classify_with_single_label_module
-from . import slmc
-
-
-classify = functools.partial(classify_with_single_label_module,
-                             single_label_module=slmc)

b6365090af94e0ba9236b0674c53c6e01bd96b22
Yota Toyama <raviqqe@gmail.com>
2017-01-05 22:05:56 +0900

    Delete unused modules

--- extenteten/classification/mlbc.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-from .multi_label import classify_with_single_label_module
-from . import slbc
-
-
-classify = functools.partial(classify_with_single_label_module,
-                             single_label_module=slbc)

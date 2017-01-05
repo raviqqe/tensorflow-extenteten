e378f1f66db59e979ed214b7ec18f7949f2cfa9a
Yota Toyama <raviqqe@gmail.com>
2017-01-05 14:31:20 +0900

    Format extenteten.assertion

--- extenteten/assertion.py
+++ extenteten/assertion.py
@@ -6,11 +6,11 @@ from .util import func_scope
 
 
 def is_int(num):
-    return isinstance(num, int) \
-        or isinstance(num, numpy.integer) \
-        or (isinstance(num, numpy.ndarray)
-            and num.ndim == 0
-            and issubclass(num.dtype.type, numpy.integer))
+    return (isinstance(num, int)
+            or isinstance(num, numpy.integer)
+            or (isinstance(num, numpy.ndarray)
+                and num.ndim == 0
+                and issubclass(num.dtype.type, numpy.integer)))
 
 
 def is_natural_num(num):
@@ -18,9 +18,9 @@ def is_natural_num(num):
 
 
 def is_natural_num_sequence(num_list, length=None):
-    return is_sequence(num_list) and \
-        all(is_natural_num(num) for num in num_list) and \
-        (length == None or len(num_list) == length)
+    return (is_sequence(num_list)
+            and all(is_natural_num(num) for num in num_list)
+            and (length == None or len(num_list) == length))
 
 
 def is_sequence(obj):

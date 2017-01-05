cfc77b2410605543ed3781408c6df2f4b4222c4e
Yota Toyama <raviqqe@gmail.com>
2017-01-05 21:06:57 +0900

    Remove log.py

--- extenteten/log.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-def init_logger(level):
-    logging.getLogger().setLevel(getattr(logging, level.upper()))
-    logging.basicConfig(format="[%(asctime)s]PID-%(process)d:"
-                               "%(module)s.%(funcName)s(): %(message)s")

e9e0b6e1f8570cc72f079ba878df1b306a4e432a
Yota Toyama <raviqqe@gmail.com>
2017-01-06 14:23:03 +0900

    s/root_width/length/g

--- extenteten/dynamic_length.py
+++ extenteten/dynamic_length.py
@@ -3,11 +3,11 @@ import tensorflow as tf
 from .util import static_rank, dimension_indices, func_scope
 
 
-__all__ = ['id_tensor_to_root_width', 'id_vector_to_length']
+__all__ = ['id_tensor_to_length', 'id_vector_to_length']
 
 
 @func_scope()
-def id_tensor_to_root_width(id_tensor, null_id=0):
+def id_tensor_to_length(id_tensor, null_id=0):
     return id_vector_to_length(tf.reduce_sum(_not_equal(id_tensor, null_id),
                                              dimension_indices(id_tensor, 2)))
 
--- extenteten/dynamic_length_test.py
+++ extenteten/dynamic_length_test.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 from .dynamic_length import *
 
 
-def test_id_tensor_to_root_width():
+def test_id_tensor_to_length():
     with tf.Session() as session, session.as_default():
         id_tensor = tf.constant([[[1], [2], [3], [0], [0]]])
-        assert id_tensor_to_root_width(id_tensor).eval() == np.array([3])
+        assert id_tensor_to_length(id_tensor).eval() == np.array([3])
 
 
 def test_id_vector_to_length():

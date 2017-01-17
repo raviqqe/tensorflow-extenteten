adb93481945024587ae7c0246283a02f570718d8
Yota Toyama <raviqqe@gmail.com>
2017-01-17 12:13:49 +0900

    Sanitize summary name

--- extenteten/summary.py
+++ extenteten/summary.py
@@ -5,7 +5,7 @@ from .util import func_scope, static_rank
 
 @func_scope()
 def tensor(variable, name=None):
-    name = name or variable.name
+    name = sanitize_summary_name(name or variable.name)
 
     mean = tf.reduce_mean(variable)
 
@@ -21,7 +21,7 @@ def tensor(variable, name=None):
 
 @func_scope()
 def image(variable, name=None):
-    name = name or variable.name
+    name = sanitize_summary_name(name or variable.name)
     rank = static_rank(variable)
 
     return tf.summary.image(
@@ -30,3 +30,7 @@ def image(variable, name=None):
          tf.expand_dims(variable, 3) if rank == 3 else
          tf.expand_dims(tf.expand_dims(variable, 0), 3)),
         max_outputs=64)
+
+
+def sanitize_summary_name(name):
+    return name.replace(':', '_')

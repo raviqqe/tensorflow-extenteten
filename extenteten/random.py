0fb31894e6a42bd588edf87ab9be90a80ba06d18
Yota Toyama <raviqqe@gmail.com>
2017-05-19 23:17:57 +0900

    Delete invalid assertion

--- extenteten/random.py
+++ extenteten/random.py
@@ -9,7 +9,6 @@ __all__ = ['sample_crop']
 
 @func_scope()
 def sample_crop(xs, n):
-    assert is_natural_num(n)
     return tf.random_crop(
         xs,
         [tf.minimum(n, tf.shape(xs)[0]), *static_shape(xs)[1:]])
